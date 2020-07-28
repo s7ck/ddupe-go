316f3efaa99c48e389c492f526e8df6582ebf741
John Durden <stickmanprime@gmail.com>
2020-07-27 22:57:06 -0500

    Saving progress. This probably doesn't compile.

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
-func getUniqueLinesFromFile(sourceFile) map {
+func getUniqueLinesFromFile(sourceFile *os.File) map[string]struct{} {
+	// We need somewhere to store the unique lines from the source file so we're
+	// using a map for it. This is a map of strings with structs as values. The
+	// structs will always be empty for two reasons:
+	//   1. No value is actually needed. The key is sufficient.
+	//   2. An empty struct takes up no memory.
+	uniqueLines := make(map[string]struct{})
+
+	numSourceLines := 0
 	scanner := bufio.NewScanner(sourceFile)
 
 	for scanner.Scan() {
@@ -18,81 +28,70 @@ func getUniqueLinesFromFile(sourceFile) map {
 			uniqueLines[thisLine] = struct{}{}
 		}
 	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d of %d lines were unique", len(uniqueLines), numSourceLines)
+
+	return uniqueLines
 }
-//testing git
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-func fileExists(path string) bool {
-	info, err := os.Stat(path)
-
-	if os.IsNotExist(err) {
-		return false
-	}
-
-	return !info.IsDir()
-}
-
-func validateArgs(argMap map) bool {
+func validateArgs(argMap map[string]struct{}) bool {
 	return true
 }
 
+// TODO -> Handle invalid CLI flags.
 func main() {
 	// TODO -> Support stdin
 
-	// We need somewhere to store the unique lines from the source file so we're
-	// using a map for it. This is a map of strings with structs as values. The
-	// structs will always be empty for two reasons:
-	//   1. No value is actually needed. The key is sufficient.
-	//   2. An empty struct takes up no memory.
-	uniqueLines := make(map[string]struct{})
-	numSourceLines := 0
-
 	// Make and parse the args we want to caputre.
-	filePathPtr := flag.String("path", "", "Path to the file from which duplicates will be removed")
-	sortPtr := flag.Bool("sort", false, "Indicates if you want to sort the output (ascending)")
-	replaceFilePtr := flag.Bool("replace", false, "Replace source file with output")
-	outputFilePtr := flag.String("output", "", "Output file")
+	// filePathPtr := flag.String("path", "", "Path to the file from which duplicates will be removed")
+	filePathPtr := flag.String("source", "", "Path to the source file from which duplicates will be removed")
+
+	// sortPtr := flag.Bool("sort", false, "Indicates if you want to sort the output (ascending)")
+	// replaceFilePtr := flag.Bool("replace", false, "Replace source file with output")
+	// outputFilePtr := flag.String("output", "", "Output file")
 
 	// Do the actual arg parsing.
 	flag.Parse()
 
-	fmt.Println("Opening " + *filePathPtr)
+	if pathIsValid(*filePathPtr) {
+		fmt.Println("Opening " + *filePathPtr)
+		sourceFile, err := os.Open(*filePathPtr)
 
-	if fileExists(*filePathPtr) {
-		file, err := os.Open(*filePathPtr)
-	}
-
-	check(err)
-
-	if *sortPtr {
-		fmt.Println("Sorting")
-	}
-
-	if *replaceFilePtr {
-		fmt.Println("Replacing source file")
-	} else {
-		if *outputFilePtr == "" {
-			fmt.Println("You must specify -ouput if you're not using -replace.")
+		if err != nil {
+			panic(err)
 		}
-	}
 
-	sourceFile, err := os.Open("test.txt")
+		// if *sortPtr {
+		// 	fmt.Println("Sorting")
+		// }
 
-	if err != nil {
-		log.Fatal(err)
-	}
+		// if *replaceFilePtr {
+		// 	fmt.Println("Replacing source file")
+		// } else {
+		// 	if *outputFilePtr == "" {
+		// 		fmt.Println("You must specify -ouput if you're not using -replace.")
+		// 	}
+		// }
 
-	defer sourceFile.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	uniqueSet := getUniqueLinesFromFile(sourceFile)
+		defer sourceFile.Close()
 
-	fmt.Printf("%d of %d lines were unique", len(uniqueLines), numSourceLines)
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		// uniqueSet := getUniqueLinesFromFile(sourceFile)
+		getUniqueLinesFromFile(sourceFile)
+	} else {
+		fmt.Printf("The source file '%v' does not exist.", *filePathPtr)
 	}
 }
---
+++ validators.go
@@ -0,0 +1,12 @@
+package main
+
+import "os"
+
+func pathIsValid(path string) bool {
+	if file, err := os.Stat(path); err == nil {
+		// The file exists, but only return true if it's not a directory.
+		return !file.IsDir()
+	}
+
+	return false
+}
